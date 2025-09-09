a9b28a0969df82de5119836aac21af3dc8ce77b0
macmann <davidkaung@gmail.com>
2025-09-09 17:00:21 +0630

    feat: proxy api requests in dev

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: { outDir: '../dist_client' },
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 });
