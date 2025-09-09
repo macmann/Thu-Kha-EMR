667e576a483a392fc4066fdb73f335fccafd8aa0
macmann <davidkaung@gmail.com>
2025-09-09 23:09:15 +0630

    style: restyle patient list page

--- client/src/pages/Patients.tsx
+++ client/src/pages/Patients.tsx
@@ -1,11 +1,5 @@
 import PatientSearch from '../components/PatientSearch';
-import PageLayout from '../components/PageLayout';
 
 export default function Patients() {
-  return (
-    <PageLayout>
-      <h1>Patients</h1>
-      <PatientSearch />
-    </PageLayout>
-  );
+  return <PatientSearch />;
 }
