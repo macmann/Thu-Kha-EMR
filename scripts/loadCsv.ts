728c2e213d1dc208f598776d36ea7cc0c02586fa
Kaung San Phyoe <davidkaung@gmail.com>
2025-09-09 15:08:12 +0630

    fix issues

--- scripts/loadCsv.ts
+++ scripts/loadCsv.ts
@@ -1,8 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { PrismaClient, Gender } from '@prisma/client';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const prisma = new PrismaClient();
+
 
 type Row = Record<string, string>;
 
